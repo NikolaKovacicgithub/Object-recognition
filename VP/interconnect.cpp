8cea39a8d32c23f9861965d5d4fd402e2b2b19ab
Nikola Kovacic <127181166+NikolaKovacicgithub@users.noreply.github.com>
2026-05-11 22:53:19 +0200

    Delete VP directory

--- VP/interconnect.cpp
+++
@@ -1,9 +0,0 @@
-#include "interconnect.hpp"
-
-void Interconnect::cpu_b_transport(tlm::tlm_generic_payload& trans, sc_core::sc_time& delay) {
-    if (trans.get_data_length() == 0) {
-        ip_socket->b_transport(trans, delay); // Forward to IP
-    } else {
-        bram_socket->b_transport(trans, delay); // Forward to BRAM
-    }
-}
--- VP/message.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef MESSAGE_HPP
-#define MESSAGE_HPP
-
-struct Message {
-    unsigned char data[3]; // Only the pixel value
-};
-
-#endif // MESSAGE_HPP
-
